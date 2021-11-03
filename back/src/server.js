dae2db3facf68e3aab5e009abc3b7f9cf264d1e0
Gonzalo Parra <gparra@VALKIMIA.COM>
2021-11-03 20:22:18 -0300

    direccionamiento sin /api

--- back/src/server.js
+++ back/src/server.js
@@ -11,13 +11,13 @@ function makeServer() {
   server.use(express.urlencoded({ extended: false }));
 
   // endpoints
-  server.use('/api/v1/users', createUserRouter());
-  server.use('/api/v1/products', createProductRouter());
-  server.use('/api/v1/paymeths', createPayMethRouter());
-  server.use('/api/v1/orders', createOrderRouter());
+  server.use('/v1/users', createUserRouter());
+  server.use('/v1/products', createProductRouter());
+  server.use('/v1/paymeths', createPayMethRouter());
+  server.use('/v1/orders', createOrderRouter());
   server.get('/', (req, res) => res.render('index', {
     title: 'App Resto',
-    message: 'Debe ingresar a las rutas correctas. Docs: /api/docs',
+    message: 'Debe ingresar a las rutas correctas. Docs: /docs',
   }));
 
   return server;
