b4eba814e257365e61a7237c27ae52306ccc721f
Gonzalo Parra <gparra@VALKIMIA.COM>
2022-02-22 18:14:38 -0300

    findone auth valida user nulo

--- back/src/controllers/routers/auth/accessDB.js
+++ back/src/controllers/routers/auth/accessDB.js
@@ -14,7 +14,7 @@ async function getUserByProvider(strategy_name, provider_user_id, provider_email
   const User = db.getModel('UserModel');
 
   // buscar por providerId y userproviderId
-  const current = await Provider.findAll({
+  const current = await Provider.findOne({
     where: {
       providerid: strategy_name,
     },
@@ -25,12 +25,14 @@ async function getUserByProvider(strategy_name, provider_user_id, provider_email
   });
   console.log(JSON.stringify(current));
 
-  if (current.length > 0) {
+  if (current) {
     // por cada producto en detalle
-    return current[0].users[0];
-  } else {
-    return createUserByProvider(strategy_name, provider_user_id, provider_email, user_name, user_lastname);
+    if (current[0].users.length > 0){
+      return current[0].users[0];      
+    }
   }
+  
+  return createUserByProvider(strategy_name, provider_user_id, provider_email, user_name, user_lastname);
 }
 
 
